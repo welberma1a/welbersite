7abb12cf8ea892ceda3272a2abfa5a938f380765
welberma1a <uwellberr7@outlook.com>
2024-02-07 14:24:10 -0300

    sorte

---
+++ random.js
@@ -0,0 +1,12 @@
+function GenerateNumber(){
+
+const min = Math.ceil(document.querySelector(".input-min").value) 
+const max = Math.floor(document.querySelector(".input-max").value)
+
+const result = Math.floor(Math.random() * (max - min +1)+min)
+
+console.log =(result)
+
+alert(result)
+
+}
